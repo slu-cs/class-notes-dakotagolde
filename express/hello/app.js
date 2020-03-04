d97187b8c85a28c00e52ed5a27bef3925a7461e0
Dakota Golde <dlgold16@stlawu.edu>
2020-03-04 13:04:59 -0500

    Express Hello-World

--- express/hello/app.js
+++ express/hello/app.js
@@ -1,1 +1,14 @@
 // Hello World web server
+const express = require('express');
+
+// Create the server
+const app = express();
+
+// Respond to one request
+app.get('/', function(request, response){
+  response.send('Hello Dakota');
+});
+
+// Start the server
+app.listen(3000);
+console.log('Server is ready.');
