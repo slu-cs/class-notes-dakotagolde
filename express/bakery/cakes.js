8829857cdb29effdff672871f2b951e492f81991
Dakota Golde <dlgold16@stlawu.edu>
2020-03-09 13:49:54 -0400

    EJS includes and expressions

--- express/bakery/cakes.js
+++ express/bakery/cakes.js
@@ -4,15 +4,23 @@ const express = require('express')
 // Create the Router
 const router = express.Router();
 
+// Pretend database collection
+const cakes = [
+  {id: 'vanilla', flavor: 'Vanilla'},
+  {id: 'chocolate', flavor: 'Chocolate'}
+];
+
 // cs-linuxlab-37.stlawu.edu:3000/cakes/
-router.get('/', function(request, response) {
-  response.send(`
-    <h1>Cakes</h1>
-    <ul>
-      <li><a href="/cakes/vanilla">Vanilla</a></li>
-      <li><a href="/cakes/chocolate">Chocolate</a></li>
-    </ul>
-    `);
+router.get('/:id', function(request, response, next) {
+
+  // Pretend database lookup
+  const cake = cakes.find(cake => cake.id === request.params.id);
+
+  if (!cake) {
+    next();   // Pass on this request
+  } else {
+      response.render('cakes/detail', {cake: cake})
+  }
 });
 
 // cs-linuxlab-37.stlawu.edu:3000/cakes/id
